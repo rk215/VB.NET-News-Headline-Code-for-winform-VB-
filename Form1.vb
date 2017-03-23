c65dfaf4297819131c48508a991e70601af15edb
rk215 <rk215@users.noreply.github.com>
2017-03-23 12:53:28 +0530

    Ad Headline Code in Vb.net
    Ad headline code for implementing headline feature (text moving from right -left ).Screen Shot  of this feature are included in this.
    
    so just download it and run it.

---
+++ Form1.vb
@@ -0,0 +1,20 @@
+﻿Public Class Form1
+    Dim t As Integer = 1 'declare globle variable t type of integer.
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Timer1.Start()  'start timer at form load.
+
+    End Sub
+
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+
+        If Label1.Left + Label1.Width > 0 Then 'to check wether left and width propertie of label control is greater than 0 or not
+            Label1.Left -= t
+            t += 1
+        Else
+            Label1.Left = Me.Width
+        End If
+
+
+    End Sub
+End Class
