1b0a2c02500538c51bf3ad7c37de32505a0b6f71
rk215 <rk215@users.noreply.github.com>
2017-03-25 12:29:17 +0530

    Circle Racing game in vb.net winform
    in this file you can be implemented circle racing game.
    watch demo video which i provided in the files.
    in here i design three circle and three lines in which circle is running .
    once we click on start/play button all circle start running.once circle reach it's end point we decide which circle is reach end point first. after that we declare that circle as winner.
    
    pretty simple and basic game for winform developer.

--- Form1.vb
+++ Form1.vb
@@ -1,20 +1,115 @@
 ﻿Public Class Form1
-    Dim t As Integer = 1 'declare globle variable t type of integer.
+    'declare 7 globle integer variale which will be use in timer control.
+    Dim i, a, a1, b, b1, c1, c As Integer
+    'FOR APPLYING RANDOM NUMBER FOR DECIDING FAIR   WINNER 
+    Dim r As New Random
 
-    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-        Timer1.Start()  'start timer at form load.
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+
+        'Enable all timer .
+        TimerBlue.Start()
+        TimerRed.Start()
+        TimerGreen.Start()
 
     End Sub
 
-    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+    'FOR RED CIRCLE CODE.
 
-        If Label1.Left + Label1.Width > 0 Then 'to check wether left and width propertie of label control is greater than 0 or not
-            Label1.Left -= t
-            t += 1
+    Private Sub TimerRed_Tick(sender As Object, e As EventArgs) Handles TimerRed.Tick
+
+        'GET THE VALUE OF OVAL OR CIRCLE  AND STOER THEM TO GLOBLE VARIABLE "a" AND "a1"
+
+        a = OvalShape1.Location.X
+        a1 = OvalShape1.Location.Y
+
+        'START OF LINESHAPE WIDTH IS "x1" AND END POINT IS "x2".
+        'SIMILIAR TO START OF LINESHAPE HEIGHT IS "Y1" AND END POINT IS "Y2".
+
+
+        'CHECK THE LINESHAPE(HORIZONTAL LINE) OF WIDTH  END POINT (X2) IS LESST THAN TO GLOBLE VARIABLE "a" OR NOT .
+
+        If a <= LineShape1.X2 Then
+
+            'HERE "r" IS RANDOM INTEGER NUMBER. WE DEFINE RANGE BETWEEN 1 AND 15.
+            'SYNTAX TO MOVE CIRCLE FROM OLD POSITION TO NEW  POSITION
+
+            ' CONTROLNAME.Location=
+            'New Point(X-axix(IN INTEGER),Y-axis(IN INTEGER))
+
+
+            OvalShape1.Location = New Point(a + (r.Next(1, 15)), a1)
+
         Else
-            Label1.Left = Me.Width
+
+            'IT MEANS CIRCLE REACH END POINT SO STOP  ALL TIMER AND DECLARE A WINNER.
+            TimerRed.Enabled = False
+            TimerBlue.Enabled = False
+            TimerGreen.Enabled = False
+            MsgBox("red is winner")
+
+        End If
+
+    End Sub
+
+    'FOR BLUE CIRCLE CODE.
+
+
+    Private Sub TimerBlue_Tick(sender As Object, e As EventArgs) Handles TimerBlue.Tick
+        'GET THE VALUE OF OVAL OR CIRCLE  AND STOER THEM TO GLOBLE VARIABLE "b" AND "b1"
+
+        b = OvalShape2.Location.X
+        b1 = OvalShape2.Location.Y
+
+        If b < LineShape2.X2 Then
+
+            'START OF LINESHAPE WIDTH IS "x1" AND END POINT IS "x2".
+            'SIMILIAR TO START OF LINESHAPE HEIGHT IS "Y1" AND END POINT IS "Y2".
+
+            ' CONTROLNAME.Location=
+            'New Point(X-axix(IN INTEGER),Y-axis(IN INTEGER))
+
+            OvalShape2.Location = New Point(b + (r.Next(1, 15)), b1)
+
+        Else
+
+            'IT MEANS CIRCLE REACH END POINT SO STOP  ALL TIMER AND DECLARE A WINNER.
+            TimerBlue.Enabled = False
+            TimerRed.Enabled = False
+            TimerGreen.Enabled = False
+
+            MsgBox("Blue is Winner")
+        End If
+
+
+
+    End Sub
+
+    'CODE FOR GREEN CIRCLE.
+    Private Sub TimerGreen_Tick(sender As Object, e As EventArgs) Handles TimerGreen.Tick
+        'GET THE VALUE OF OVAL OR CIRCLE  AND STOER THEM TO GLOBLE VARIABLE "c" AND "c1"
+
+
+        c = OvalShape3.Location.X
+        c1 = OvalShape3.Location.Y
+
+        If c <= LineShape3.X2 Then
+            'START OF LINESHAPE WIDTH IS "x1" AND END POINT IS "x2".
+            'SIMILIAR TO START OF LINESHAPE HEIGHT IS "Y1" AND END POINT IS "Y2".
+
+            ' CONTROLNAME.Location=
+            'New Point(X-axix(IN INTEGER),Y-axis(IN INTEGER))
+            OvalShape3.Location = New Point(c + (r.Next(1, 15)), c1)
+
+        Else
+            'IT MEANS CIRCLE REACH END POINT SO STOP  ALL TIMER AND DECLARE A WINNER.
+            TimerBlue.Enabled = False
+            TimerRed.Enabled = False
+            TimerGreen.Enabled = False
+            MsgBox("Green is Winner")
+
         End If
 
 
     End Sub
+
 End Class
